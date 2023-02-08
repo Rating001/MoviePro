df576753877a8de52a03c0784f6b9204c9c6b2b1
Rating001 <106230549+Rating001@users.noreply.github.com>
2023-02-07 21:23:05 -0800

    Created the ImageService and registered it.

--- Services/ImageService.cs
+++ Services/ImageService.cs
@@ -4,19 +4,36 @@ namespace MoviePro.Services
 {
     public class ImageService : IImageService
     {
+        private readonly IHttpClientFactory _httpClient;
+
+        public ImageService(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public string DecodeImage(byte[] poster, string contentType)
         {
-            throw new NotImplementedException();
+            if (poster is null || contentType is null) return null;
+            return $"data:image/{contentType};base64,{Convert.ToBase64String(poster)}";
         }
 
-        public Task<byte[]> EncodeImageAsync(IFormFile poster)
+        public async Task<byte[]> EncodeImageAsync(IFormFile poster)
         {
-            throw new NotImplementedException();
+            if (poster is null) return null;
+            using var stream = new MemoryStream() ;
+            await poster.CopyToAsync(stream);
+            return stream.ToArray();
         }
 
-        public Task<byte[]> EncodeImageURLAsyc(string imageURL)
+        public async Task<byte[]> EncodeImageURLAsyc(string imageURL)
         {
-            throw new NotImplementedException();
+            var client = _httpClient.CreateClient();
+            var response = await client.GetAsync(imageURL);
+
+            using Stream stream = await response.Content.ReadAsStreamAsync();
+            var ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+            return ms.ToArray();
         }
     }
 }
