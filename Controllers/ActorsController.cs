53776ce718057813e684a824501577fd9d2a39f4
Rating001 <106230549+Rating001@users.noreply.github.com>
2023-02-10 20:18:08 -0800

    Created and completed the ActorsController.

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviePro.Services.Interfaces;
+
+namespace MoviePro.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _tmdbMappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService tmdbMappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _tmdbMappingService = tmdbMappingService;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _tmdbMappingService.MapActorDetail(actor);
+            return View(actor);
+        }
+    }
+}
